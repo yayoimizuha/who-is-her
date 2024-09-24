1030f26afb8c12c2b3b7db1ee018c9a55a3176be
yayoimizuha <poko.peko.tomo@icloud.com>
2024-09-25 02:39:26 +0900

    Add Google Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,70 @@
 <script>
-	import Header from './Header.svelte';
-	import '../app.css';
+    import Header from './Header.svelte';
+    import '../app.css';
+
+
 </script>
 
 <div class="app">
-	<Header />
+    <Header/>
 
-	<main>
-		<slot />
-	</main>
+    <main>
+        <slot/>
+    </main>
 
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
+    <footer>
+        <p>制作: <a href="https://x.com/yayoi_mizuha" target="_blank">@yayoi_mizuha</a> (画像の著作権はアップフロントインターナショナル社に属します。)</p>
+    </footer>
+
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-BPY1S17CTR"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'G-BPY1S17CTR');
+    </script>
+
 </div>
 
 <style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
 
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: 1rem;
+        width: 100%;
+        max-width: 64rem;
+        margin: 0 auto;
+        box-sizing: border-box;
+    }
 
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
+    footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 12px;
+    }
 
-	footer a {
-		font-weight: bold;
-	}
+    footer a {
+        font-weight: bold;
+    }
 
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
-		}
-	}
+    @media (min-width: 480px) {
+        footer {
+            padding: 12px 0;
+        }
+    }
 </style>
